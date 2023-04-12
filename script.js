76d50d6316a1ca7ee557a43f4d2dd6635cf7562e
wannacry3030 <wannacry.bat@proton.me>
2023-04-12 16:28:44 -0300

    att

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 'use strict';
 
-console.log(me);
-console.log(job);
-console.log(year);
+// VARIAVEIS
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
 var me = 'lucas';
 let job = 'developer';
 const year = 1996;
+
+// FUNÇÕES
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+//Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
